d568f2bb4870aec401ba8fdd9289c5e3e1ad456d
UpSage <aton.christopher05@gmail.com>
2023-12-21 20:57:49 +0000

    Initial changes for UI Components using widget

---
+++ Block/Widget/View.php
@@ -0,0 +1,9 @@
+<?php
+
+ namespace UpSage\SlickSliderWidget\Block\Widget;
+
+ use Magento\Widget\Block\BlockInterface;
+ 
+ class View extends \Grasch\AdminUi\Block\Widget\AbstractWidget implements BlockInterface {
+  protected $_template = 'widget/view.phtml';
+ }
